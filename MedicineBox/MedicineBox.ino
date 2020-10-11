07014cfe0e0bd0949ef6adf949bb229ea3e2032d
Juul <j.l.peters@student.tue.nl>
2020-10-11 11:12:16 +0200

    Music code update + FirebaseData start

--- MedicineBox/MedicineBox.ino
+++ MedicineBox/MedicineBox.ino
@@ -1,73 +1,17 @@
-#include <SPI.h>
-#include <TMRpcm.h> // The library to play the audio files
-TMRpcm Memoria;
-#include <SD.h> // SD library
-#define SD_ChipSelectPin 4 // Select the SS pin for the SD module
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 #include <Servo.h> //Servo library
-#include "pitches.h"
+//Include Firebase ESP8266 library
+#include "FirebaseESP8266.h"
+//Include ESP8266WiFi.h
+#include <ESP8266WiFi.h>
 
-int melody[] = {
-NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_A4,
-NOTE_G4, NOTE_C5, NOTE_AS4, NOTE_A4,                   
-NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_DS4, NOTE_D4,
-NOTE_C4, NOTE_D4,0,                                 
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
 
-NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_A4,
-NOTE_G4, NOTE_C5, NOTE_D5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_A4,      //29               //8
-NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_DS4, NOTE_D4,
-NOTE_C4, NOTE_D4,0,                                       
-
-NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_DS5, NOTE_D5,
-NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_C5,
-NOTE_C4, NOTE_D4, NOTE_DS4, NOTE_FS4, NOTE_D5, NOTE_C5,
-NOTE_AS4, NOTE_A4, NOTE_C5, NOTE_AS4,             //58
-
-NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_DS5, NOTE_D5,
-NOTE_C5, NOTE_D5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_C5, NOTE_G4,
-NOTE_A4, 0, NOTE_AS4, NOTE_A4, 0, NOTE_G4,
-NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, 0,
-
-NOTE_C4, NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_DS4,
-NOTE_C4, NOTE_D4, 0,
-NOTE_C4, NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_DS4,
-NOTE_C4, NOTE_D4, END
-
-};
-
-// note durations: 8 = quarter note, 4 = 8th note, etc.
-int noteDurations[] = {       //duration of the notes
-8,4,8,4,
-4,4,4,12,
-4,4,4,4,4,4,
-4,16,4,
-
-8,4,8,4,
-4,2,1,1,2,1,1,12,
-4,4,4,4,4,4,
-4,16,4,
-
-4,4,4,4,4,4,
-4,4,4,12,
-4,4,4,4,4,4,
-4,4,4,12,
-
-4,4,4,4,4,4,
-2,1,1,2,1,1,4,8,4,
-2,6,4,2,6,4,
-2,1,1,16,4,
-
-4,8,4,4,4,
-4,16,4,
-4,8,4,4,4,
-4,20,
-};
-
-
-
+FirebaseData firebaseData;
 Servo myservo; // servo object to control a servo
-
-int speed=90; // higher value, slower notes
-int played=0; // variable to know if reminder has been played
 
 int pos=90; // variable to store the servo position
 int rotate=10 //variable to store the servo rotation time
@@ -75,66 +19,61 @@ int rotate=10 //variable to store the servo rotation time
 const int buttonPin = 2; // the number of the pushbutton pin
 int buttonState = 0; //variable for reading the pushbutton status
 
+void begin(const String &host, const String &auth = "")
+String pushInt(const String &path, int value)
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600)
+  //Setup Firebase credential in setup:
+  Firebase.begin(".firebase.com","");
+
+  //Setup of music:
+  mySoftwareSerial.begin(9600);
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  myDFPlayer.volume(15);
+  
+  // initialize serial communication:
+  Serial.begin(115200);
+  
   // attaches the servo on pin 8 to the servo object
   myservo.attach(8);
+  
   //initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
-  // If the SS pin is in a LOW state it will send a Fail message:
-  if (!SD.begin(SD_ChipSelectPin)){
-    Serial.println("SD fail");
-    return;
-  }
-  Memoria.speakerPin = 9; // The pin where the speaker is located
+
 }
 
 void loop() {
-  // read value from app somehow to know if medicine needs to be taken.
-  if (played==0) {
-    Memoria.setVolume(5); // Set volume from 1 to 7
-    Memoria.quality(1); // Set to 0 or 1, 1 is better quality
-    Memoria.play("music.wav"; //Name of the audio file
-    delay(10000); //Delay, should be at least of the same length as the audio file
-
-
-
-
-
-    /*
-    for (int thisNote = 0; melody[thisNote]!=-1; thisNote++) {
-
-    int noteDuration = speed*noteDurations[thisNote];
-    tone(3, melody[thisNote],noteDuration*.95);
-    
-    delay(noteDuration);
-    
-    noTone(3);
-    }
-    */
-    }
-
   
+  // Check value from database to see if medicine needs to be taken
+  if (Firebase.getInt(firebaseData, )){
+    if (firebaseData.dataType() == "int"){
+      med = firebase.intData();
+    }
+  } else {
+    Serial.println(firebaseData.errorReason());
+  }
+  // read value from app database to know if medicine needs to be taken and play music.
+  if (med=1) {
+    myDFPlayer.play(1)
+  }
+
+
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
 
 
   //Activate motor when button is pressed and app gives a signal to
-  if (buttonState == HIGH) {
+  if (buttonState == HIGH and med == 1) {
     myservo.write(pos);           // tell servo to go to rotate at speed written in variable 'pos'
     delay(rotate);                // the servo will continue to rotate at this speed for 'rotate' seconds
     
   }
-
-  
-  // Detect input of gas sensor:
-  gasThreshold = 0;
-  int gasValue = analogRead(A0);
-
-  if gasValue > gasThreshold;
-    Serial.println("teveel gas");
-
 
 
 
